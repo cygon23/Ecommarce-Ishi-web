db3424ee14ef5d4b64ca3a292a84b1df79e6fabd
cygon23 <rweyemamu088@gmail.com>
2024-10-21 09:17:07 +0300

    product cart feature completes

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,9 +1,9 @@
 <header class="header_section">
     <nav class="navbar navbar-expand-lg custom_nav-container ">
         <a class="navbar-brand" href="index.html">
-            {{-- <span>
+            <span>
                 Giftos
-            </span> --}}
+            </span>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
