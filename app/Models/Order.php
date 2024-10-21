a668e9ee1df39fc0e64d0fa72a7171cb8c70439d
cygon23 <rweyemamu088@gmail.com>
2024-10-21 10:37:39 +0300

    orders display for admin completes

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
+
+    public function product()
+    {
+        return $this->hasOne(Product::class, 'id', 'product_id');
+    }
 }
