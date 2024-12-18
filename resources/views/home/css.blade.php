23c94219c4d504c4056e020b308f5eec8c3976cd
cygon23 <rweyemamu088@gmail.com>
2024-12-18 19:01:58 +0300

    minor changes

--- resources/views/home/css.blade.php
+++ resources/views/home/css.blade.php
@@ -29,7 +29,7 @@
 
 
     <!-- Bootstrap 5 CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
 
     <!-- FontAwesome for the search icon -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
